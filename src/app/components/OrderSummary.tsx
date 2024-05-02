a218ac16a0d99a10db23f476c9315d365f07abee
leogytis <leogytis@gmail.com>
2024-05-02 11:14:22 +0300

    Order Summary complete

--- src/app/components/OrderSummary.tsx
+++ src/app/components/OrderSummary.tsx
@@ -27,7 +27,7 @@ export const OrderSummary = () => {
 				<div>$299.97</div>
 			</div>
 
-			<article className="w-full flex flex-col pt-6">
+			<article className="w-full flex flex-col gap-4 pt-6">
 				<div className="flex justify-center">
 					<div className="w-full flex justify-center relative ">
 						<div className="absolute top-1/2 gray-line-separator"></div>
@@ -35,32 +35,33 @@ export const OrderSummary = () => {
 					</div>
 				</div>
 
-				<div className="flex border-2 border-red-800 p-4 mt-10">
-					<div className="flex items-center p-2">Icon</div>
-					<div className="flex flex-col p-2">
+				<div className="flex items-start">
+					<Image src="/images/cashback.svg" alt="cashback_icon" width={32} height={32} className="mr-4" />
+					<div className="flex flex-col gap-2 text-sm">
 						<h4 className="font-bold">90-Day Money Back Guarantee</h4>
-						<p>
+						<p className="text-gray_medium">
 							We love our products and we're confident you will too! If you're not in love with your
 							LogoIpsum product, our easy return and refund policy is designed to make things as easy as
 							possible for you.
 						</p>
 					</div>
 				</div>
-				<div className="flex border-2 border-red-800 p-4">
-					<div className="flex items-center p-2">Icon</div>
-					<div className="flex flex-col p-2">
+
+				<div className="flex items-start">
+					<Image src="/images/rating.svg" alt="rating_icon" width={32} height={32} className="mr-4" />
+					<div className="flex flex-col gap-2 text-sm">
 						<h4 className="font-bold">Over 75,000+ Happy Customer</h4>
-						<p>
+						<p className="text-gray_medium">
 							Everyone that tries LogoIpsum says it’s a must-have. We invest a lot of love and care into
 							making our products, so you can enjoy seeing results when using it.
 						</p>
 					</div>
 				</div>
-				<div className="flex border-2 border-red-800 p-4">
-					<div className="flex items-center p-2">Icon</div>
-					<div className="flex flex-col p-2">
+				<div className="flex items-start">
+					<Image src="/images/customer.svg" alt="customer_icon" width={32} height={32} className="mr-4" />
+					<div className="flex flex-col gap-2 text-sm">
 						<h4 className="font-bold">Professional Customer Support</h4>
-						<p>
+						<p className="text-gray_medium">
 							Our customer service works 24/7 for your satisfaction. Feel free to reach out to us anytime.
 						</p>
 					</div>

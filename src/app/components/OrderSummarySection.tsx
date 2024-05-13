4150bd2dd978a0724a6c7520ce58e4a66c945620
leogytis <leogytis@gmail.com>
2024-05-13 09:34:09 +0300

    toggler

--- src/app/components/OrderSummarySection.tsx
+++ src/app/components/OrderSummarySection.tsx
@@ -2,35 +2,57 @@
 import Image from "next/image";
 import {ChooseUsSection} from "./ChooseUsSection";
 import {ScreenSize} from "../utils/ScreenSize";
-import {OrderToggler} from "../utils/OrderToggler";
+import {IoIosArrowDown, IoIosArrowUp} from "react-icons/io";
+import {useState} from "react";
 
 export const OrderSummarySection = () => {
+	const [isOverviewOpen, setIsOverviewOpen] = useState(false);
+
+	const toggleOverview = () => {
+		setIsOverviewOpen(!isOverviewOpen);
+	};
+
 	const isMobile = ScreenSize();
 	return (
 		<section className="w-full lg:w-5/12 flex flex-col gap-4 bg-gray_background p-4 lg:pr-0 lg:py-[40px] lg:pl-[38px]">
-			{isMobile && <OrderToggler />}
-			<div className="flex justify-between items-center">
-				<div className="flex items-center">
-					<div className="relative flex items-center">
-						<div className="absolute top-0 left-full transform -translate-y-1/2 -translate-x-1/2 font-sm text-white rounded-full bg-gray_medium px-[7px]">
-							3
+			<section className="w-full border-b border-gray_border">
+				<div className="flex justify-between items-center ">
+					<div className="flex gap-2 items-center">
+						Order overview
+						<div className="cursor-pointer" onClick={toggleOverview}>
+							{isOverviewOpen ? <IoIosArrowUp /> : <IoIosArrowDown />}
 						</div>
-						<Image src="/images/product.png" alt="Product" width={64} height={64} />
 					</div>
-					<h3 className="text-sm font-bold px-4">LogoImpsum PIL</h3>
+
+					<div>$299.97</div>
 				</div>
-				<div className="text-sm font-medium">$299.97</div>
-			</div>
-			<div className="gray-line-separator"></div>
-			<div className="flex justify-between items-center text-sm">
-				<div>Subtotal</div>
-				<div>$299.97</div>
-			</div>
-			<div className="gray-line-separator"></div>
-			<div className="flex justify-between items-center text-lg font-bold">
-				<div>Total</div>
-				<div>$299.97</div>
-			</div>
+			</section>
+			{isOverviewOpen ? (
+				<section className="flex flex-col gap-4">
+					<div className="flex justify-between items-center">
+						<div className="flex items-center">
+							<div className="relative flex items-center">
+								<div className="absolute top-0 left-full transform -translate-y-1/2 -translate-x-1/2 font-sm text-white rounded-full bg-gray_medium px-[7px]">
+									3
+								</div>
+								<Image src="/images/product.png" alt="Product" width={64} height={64} />
+							</div>
+							<h3 className="text-sm font-bold px-4">LogoImpsum PIL</h3>
+						</div>
+						<div className="text-sm font-medium">$299.97</div>
+					</div>
+					<div className="gray-line-separator"></div>
+					<div className="flex justify-between items-center text-sm">
+						<div>Subtotal</div>
+						<div>$299.97</div>
+					</div>
+					<div className="gray-line-separator"></div>
+					<div className="flex justify-between items-center text-lg font-bold">
+						<div>Total</div>
+						<div>$299.97</div>
+					</div>
+				</section>
+			) : null}
 			{!isMobile && <ChooseUsSection />}
 		</section>
 	);
--- src/app/utils/OrderToggler.tsx
+++ src/app/utils/OrderToggler.tsx
@@ -1,14 +1,19 @@
+import {useState} from "react";
 import {IoIosArrowUp, IoIosArrowDown} from "react-icons/io";
 
 export const OrderToggler = () => {
+	const [isOverviewOpen, setIsOverviewOpen] = useState(false);
+
+	const toggleOverview = () => {
+		setIsOverviewOpen(!isOverviewOpen);
+	};
+
 	return (
 		<section className="w-full border-b border-gray_border">
 			<div className="flex justify-between items-center ">
 				<div className="">Order overview</div>
-				<div>
-					<IoIosArrowUp />
-					<IoIosArrowDown />
-				</div>
+				<div>{isOverviewOpen ? <IoIosArrowUp /> : <IoIosArrowDown />}</div>
+				<div>$299.97</div>
 			</div>
 		</section>
 	);
